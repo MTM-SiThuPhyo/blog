c8aaacbdb53f45251b0e6a61f79b4d63d789d73a
SiThuPhyo-scm <scm.sithuphyo@gmail.com>
2022-06-18 17:10:43 +0630

    Blade partial and update

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,62 +1,29 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.master')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-</head>
+@section('title', 'Category Page List')
 
-<body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/posts">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/categories">Category List</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+@section('content')
 
-    <div class="container mt-5">
-        <div class="d-flex justify-content-end">
-            <a class="btn btn-success" href="/categories/create">Create A Category</a>
-        </div>
-        @foreach ($categories as $category)
-            <div>
-                <h3><a href="/categories/{{ $category->id }}">{{ $category->name }}</a></h3>
-                January 1, 2021 by Mark
-                <div class="d-flex justify-content-end">
-                    <a href="/categories/{{ $category->id }}/edit/" class="btn btn-outline-success">Edit</a>
-                    <form action="/categories/{{ $category->id }}"
-                        method="POST"
-                        onsubmit="return confirm('Are you sure to delete?')">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit" class="btn btn-outline-danger ms-2">Delete</button>
-                    </form>
-                </div>
-            </div>
-
-            <hr>
-        @endforeach
-
+    <div class="d-flex justify-content-end">
+        <a class="btn btn-success" href="/categories/create">Create A Category</a>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
-    </script>
-</body>
-</html>
+    @foreach ($categories as $category)
+        <div>
+            <h3><a href="/categories/{{ $category->id }}">{{ $category->name }}</a></h3>
+            January 1, 2021 by Mark
+            <div class="d-flex justify-content-end">
+                <a href="/categories/{{ $category->id }}/edit/" class="btn btn-outline-success">Edit</a>
+                <form action="/categories/{{ $category->id }}"
+                    method="POST"
+                    onsubmit="return confirm('Are you sure to delete?')">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-outline-danger ms-2">Delete</button>
+                </form>
+            </div>
+        </div>
+
+        <hr>
+    @endforeach
+    
+@endsection
